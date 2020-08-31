b97892cd58ff5c472dc187f8219cecd02daf522e
Jamie Brindle <jamie.brindle7.work@gmail.com>
2020-08-31 18:23:19 +0100

    Updated nuspec

--- nuget-push.bat
+++ nuget-push.bat
@@ -1,4 +1,4 @@
 cd JBToolkit
 del /q /f *.nupkg
 nuget.exe pack -IncludeReferencedProjects -properties Configuration=Release
-nuget.exe push JBToolkit.1.0.1.3.nupkg -Source https://api.nuget.org/v3/index.json -Timeout 300
+nuget.exe push JBToolkit.1.0.1.4.nupkg -Source https://api.nuget.org/v3/index.json -Timeout 300
