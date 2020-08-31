0a425e100d613336ec788f71a39d13ed285f266a
Jamie Brindle <jamie.brindle7.work@gmail.com>
2020-08-31 16:06:37 +0100

    Still getting nuspec sorted

--- nuget-push.bat
+++ nuget-push.bat
@@ -1,4 +1,4 @@
 cd JBToolkit
 del /q /f *.nupkg
 nuget.exe pack -IncludeReferencedProjects -properties Configuration=Release
-nuget.exe push JBToolkit.1.0.0.2 -Source https://api.nuget.org/v3/index.json -Timeout 300
+nuget.exe push JBToolkit.1.0.1.2.nupkg -Source https://api.nuget.org/v3/index.json -Timeout 300
